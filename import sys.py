3f348b1c97327871e40871398faeb0fa8685fb99
MeowWnag <119922838+MeowWnag@users.noreply.github.com>
2024-04-20 02:48:57 +0800

    Add files via upload

---
+++ import sys.py
@@ -0,0 +1,28 @@
+import pystray
+from pystray import MenuItem as item
+from PIL import Image
+import webbrowser  # 匯入 webbrowser 模組
+import subprocess  # 匯入 subprocess 模組
+
+# 定義開啟網站的函數
+def open_website(icon):
+    webbrowser.open('https://youtu.be/dQw4w9WgXcQ?si=DyW9I3U5ojTOsDuh')
+
+# 定義開啟應用程式的函數
+def open_application(icon):
+    # 示範開啟記事本應用程式
+    subprocess.Popen(["notepad.exe"])
+
+# 建立選單項目
+menu = (item('開啟網站', open_website),
+        item('開啟應用程式', open_application),
+        item('離開', lambda icon, item: icon.stop()))
+
+# 載入圖示
+image = Image.open("D:\\Users\\charles\\下載\\高師.png")
+
+# 建立系統列圖示
+icon = pystray.Icon("name", image, "title", menu)
+
+# 啟動系統列圖示
+icon.run()
